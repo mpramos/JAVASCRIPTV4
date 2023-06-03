dd536c7ef155746a7774fd78bddf623b9076bce3
mramos.dev423 <mramosg@fcpn.edu.bo>
2023-06-03 18:43:54 -0400

    se agregaron tres lineas

--- index.js
+++ index.js
@@ -13,5 +13,7 @@ let nombre1="Juana"
 console.log (nombre);console.log (nombre1)
 
 
-
+console.log('test'.split(1, 2)); // -> 'es'
+let str="java🎇script🎇language🎇evolutech"
+console.log(str.split('🎇')); // -> ['java', 'script', 'language']
 
--- variables.js
+++ variables.js
@@ -24,3 +24,45 @@ console.log(alto)
 console.log(height)
 
 
+let message1 = "El buque 'Mars' hizo escala en el puerto.";
+let message2 = 'El ciclón "Cilida" pasará cerca de Mauritius jajaja';
+let message3=  `La mayoria de los alumnos de JavaScript 
+
+
+                            hicieron la tarea de ver el video`
+//altGr+}
+console.log(message1) // -> El buque 'Mars' hizo escala en el puerto.
+console.log(message2) // -> El ciclón "Cilida" pasará cerca de Mauritius.
+console.log(message3)
+console.log(typeof message3)
+
+let country = "Malawi";
+let continent = "Africa";
+
+let sentence = `${country} se ubica en ${continent}.`;
+console.log(sentence); // -> Malawi se ubica en Africa.
+// let country = "Malawi"
+// let continent = "Africa"
+
+// let sentence ='jaja' +country + ' se ubica en '+continent +'jdhd'
+// console.log(sentence) // -> Malawi se ubica en Africa.
+
+
+const num = 42;
+
+const strFromNum1 = String(num);
+const strFromNum2 = String(8);
+const strFromBool = String(true);
+const numFromStr = Number("312");
+const boolFromNumber = Boolean('jaja');
+console.log(strFromNum1);
+console.log(typeof strFromNum1);
+console.log(strFromNum2);
+console.log(typeof strFromNum2);
+console.log(strFromBool);
+console.log(typeof strFromBool);
+console.log(numFromStr);
+console.log(typeof numFromStr);
+
+console.log(boolFromNumber);
+console.log(typeof boolFromNumber);
